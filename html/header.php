989c45a517f0c8feb3dfb9bcfcf2319636c11db5
Kevin Whalen <KWhalen2@kent.edu>
2013-04-14 20:08:17 -0400

    removed old html setup and directory

--- html/header.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />';
-//echo '<link rel="stylesheet" type="text/css" href="styles/main.css" />
-//echo '<link rel="icon" type="image/x-icon" href="favicon.ico" />';
-?>
