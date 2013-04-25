939ed85b33c16a917f6d236989d55bd546f5c3ff
widma1 <widma1@yahoo.com>
2013-04-25 19:43:06 -0400

    .

--- indexStatements.sql
+++ indexStatements.sql
@@ -1,1 +1,7 @@
-CREATE INDEX some_index_name ON table_name (attribute_name(#));
+CREATE INDEX Find_Person ON Person (User_ID);
+CREATE INDEX Find_Guests_List ON Guests_List (Giver_ID);
+CREATE INDEX Find_Gift ON Gift (Event_ID);
+CREATE INDEX Find_Item ON Item (Item_ID);
+CREATE INDEX Find_Contact_Info ON Contact_Info (User_ID);
+CREATE INDEX Find_Address ON Address (Location_ID);
+CREATE INDEX Find_Events ON Events (User_ID);
