b41a3cee3e1b6b490ec52cc26876c8b37b4fe296
widma1 <widma1@yahoo.com>
2013-05-06 15:15:16 -0400

    data def. and dumy data insert fixed

--- SQL/dataDefinitionSchema.sql
+++ SQL/dataDefinitionSchema.sql
@@ -1,20 +1,20 @@
 create table Person
 	(
-	User_ID		int not null AUTO_INCREMENT,
-	User_Name	varchar(15),
+	User_ID			int not null AUTO_INCREMENT,
+	User_Name		varchar(15),
 	User_Password	varchar(20),
-	F_Name		varchar(15),
-	L_Name		varchar(15),
-	M_Name		varchar(15),
-	DOB		Date,
+	F_Name			varchar(15),
+	L_Name			varchar(15),
+	M_Name			varchar(15),
+	DOB				Date,
 	primary key (User_ID)
 	);
 
 create table Guests_List
 	(
-	Giver_ID	varchar(15) not null,
-	Receiver_ID	varchar(15) not null,
-	Event_ID	varchar(15) not null,
+	Giver_ID	int not null,
+	Receiver_ID int not null,
+	Event_ID	int not null,
 	Coming 		tinyint(1),
 	Gifting		tinyint(1),
 	primary key (Giver_ID, Receiver_ID, Event_ID)
@@ -24,7 +24,7 @@ create table Gift
 	(
 	Item_ID		varchar(20) not null,
 	Event_ID	int not null,
-	Giver_ID	varchar(20),
+	Giver_ID	int,
 	Price		numeric(8, 2),
 	Quantity 	numeric(3, 0),
 	primary key (Item_ID, Event_ID)
@@ -32,9 +32,9 @@ create table Gift
 
 create table Item
 	(
-	Item_ID		varchar(20) not null, /* upc */
-	Department	varchar(25), /* assigned department */
-	Title		varchar(150), /* RPC 'description' */
+	Item_ID			varchar(20) not null, /* upc */
+	Department		varchar(25), /* assigned department */
+	Title			varchar(150), /* RPC 'description' */
 	Classification	varchar(25), /* RPC 'size' (further details) */
 	primary key(Item_ID)
 	);
@@ -50,23 +50,23 @@ create table Contact_Info
 
 create table Address
 	(
-	Location_ID	int not null AUTO_INCREMENT,
+	Location_ID		int not null AUTO_INCREMENT,
 	Location_Name	varchar(15),
-	City		varchar(15),
-	Street_Name	varchar(20),
+	City			varchar(15),
+	Street_Name		varchar(20),
 	Street_Number	numeric(7, 0),
-	Street_Ext	varchar(20),
-	State		varchar(15),
+	Street_Ext		varchar(20),
+	State			varchar(15),
 	primary key (Location_ID)
 	);
 
 create table Events
 	(
 	Event_ID	int not null AUTO_INCREMENT,
-	User_ID		varchar(15),
+	User_ID		int,
 	Event_Name	varchar(30),
 	Event_Date	Date,		
-	Location_ID	varchar(20),
+	Location_ID	int,
 	primary key (Event_ID)
 	); 
 
--- SQL/insertDummyData.sql
+++ SQL/insertDummyData.sql
@@ -1,41 +1,75 @@
-insert into Person 
-	('User_Name','User_Password','F_Name','L_Name', 'M_Name', 'DOB') 
-	values 
-	('Bill5', 'password', 'Bill', 'Evan', 'Smith', '1985-04-21');
-insert into Person ('User_ID','User_Name','User_Password','F_Name','L_Name', 'M_Name', 'DOB') values (00002, 'L', 'password1', 'Laverne', 'Anny', 'Smith', '1977-09-23');
-insert into Person ('User_ID','User_Name','User_Password','F_Name','L_Name', 'M_Name', 'DOB') values (00003, 'N', 'password1', 'Nick', 'Kevin', 'Smith', '1978-08-15');
-insert into Person ('User_ID','User_Name','User_Password','F_Name','L_Name', 'M_Name', 'DOB') values (00004, 'Ti3', 'password1', 'Tim', 'Eric', 'Smith', '1986-05-05');
-insert into Person ('User_ID','User_Name','User_Password','F_Name','L_Name', 'M_Name', 'DOB') values (00005, 'Todd9001', 'password1', 'Todd', 'John', 'McDonald', '1936-06-23');
-
-insert into Guests_List ('Giver_ID', 'Receiver_ID', 'Event_ID', 'Coming', 'Gifting') values ('00002', '00001', '00001', 1, 1);
-insert into Guests_List ('Giver_ID', 'Receiver_ID', 'Event_ID', 'Coming', 'Gifting') values ('00003', '00001', '00001', 0, 1);
-insert into Guests_List ('Giver_ID', 'Receiver_ID', 'Event_ID', 'Coming', 'Gifting') values ('00004', '00001', '00001', 1, 1);
-insert into Guests_List ('Giver_ID', 'Receiver_ID', 'Event_ID', 'Coming', 'Gifting') values ('00005', '00001', '00001', 1, 0);
-
-insert into Events ('Event_ID', 'User_ID', 'Event_Name', 'Event_Date', 'Location_ID') values ('00001', '00001', 'Bill Birthday', '2013-04-21', 00001);
-
-insert into Gift ('User_ID','Event_ID','Giver_ID','Price','Quantity') values ('00001', 00001, '320', 1); 
-insert into Gift ('User_ID','Event_ID','Giver_ID','Price','Quantity') values ('00002', 00001, '15', 4);
-insert into Gift ('User_ID','Event_ID','Giver_ID','Price','Quantity') values ('00003', 00001, '150', 1);
-insert into Gift ('User_ID','Event_ID','Giver_ID','Price','Quantity') values ('00004', 00001, '1', 15);
-
-insert into Item ('Item_ID', 'Department', 'Title', 'Classification') values ('00001', 'Electronics', 'Xbox 360', 'Elite');
-insert into Item ('Item_ID', 'Department', 'Title', 'Classification') values ('00002', 'Clothes', 'T-Shirts', 'Medium');
-insert into Item ('Item_ID', 'Department', 'Title', 'Classification') values ('00003', 'Music', 'Guitar', 'Bass');
-insert into Item ('Item_ID', 'Department', 'Title', 'Classification') values ('00004', 'Food', 'Chocolate', 'Milk');
+insert into Person ('User_Name', 'User_Password', 'F_Name',  'L_Name', 'M_Name',   'DOB') 
+            values ('Bill5',     'password',      'Bill',    'Evan',   'Smith',    '1985-04-21');
+insert into Person ('User_Name', 'User_Password', 'F_Name',  'L_Name', 'M_Name',   'DOB') 
+	        values ('L',         'password1',     'Laverne', 'Anny',   'Smith',    '1977-09-23');
+insert into Person ('User_Name', 'User_Password', 'F_Name',  'L_Name', 'M_Name',   'DOB') 
+	        values ('N',         'password1',     'Nick',    'Kevin',  'Smith',    '1978-08-15');
+insert into Person ('User_Name', 'User_Password', 'F_Name',  'L_Name', 'M_Name',   'DOB') 
+	        values ('Ti3',       'password1',     'Tim',     'Eric',   'Smith',    '1986-05-05');
+insert into Person ('User_Name', 'User_Password', 'F_Name',  'L_Name', 'M_Name',   'DOB') 
+	        values ('Todd9001',  'password1',     'Todd',    'John',   'McDonald', '1936-06-23');
 
 
-insert into Contact_Info ('User_ID', 'Location_ID', 'Phone-Num', 'Email') values (00001, 00001, '440-568-9124', 'bill10@cox.net');
-insert into Contact_Info ('User_ID', 'Location_ID', 'Phone-Num', 'Email') values (00002, 00002, '440-569-8742', 'laverne@cox.net');
-insert into Contact_Info ('User_ID', 'Location_ID', 'Phone-Num', 'Email') values (00003, 00003, '440-578-9612', 'hi1234@yahoo.com');
-insert into Contact_Info ('User_ID', 'Location_ID', 'Phone-Num', 'Email') values (00004, 00004, '216-852-9461', 'ti@cox.net');
-insert into Contact_Info ('User_ID', 'Location_ID', 'Phone-Num', 'Email') values (00005, 00005, '216-963-4561', 'Imlovingit@yahoo.com');
 
-insert into Address ('Location_ID', 'Location_Name', 'City', 'Street_Name', 'Street_Number','Street_Ext','State') values (00001, 'Home', 'Cleveland', 'Glenside','26993', 'Brunswick Circle', 'Ohio');
-insert into Address ('Location_ID', 'Location_Name', 'City', 'Street_Name', 'Street_Number','Street_Ext','State') values (00002, 'Home', 'Cleveland', 'Glenside','26973', 'Brunswick Circle', 'Ohio');
-insert into Address ('Location_ID', 'Location_Name', 'City', 'Street_Name', 'Street_Number','Street_Ext','State') values (00003, 'Home', 'Cleveland', 'Glenside','26983', 'Brunswick Circle', 'Ohio');
-insert into Address ('Location_ID', 'Location_Name', 'City', 'Street_Name', 'Street_Number','Street_Ext','State') values (00004, 'Work', 'Toledo', 'Sterns','26993', 'Brunswick Circle', 'Ohio');
-insert into Address ('Location_ID', 'Location_Name', 'City', 'Street_Name', 'Street_Number','Street_Ext','State') values (00005, 'Home', 'Cleveland', 'Glenside', '26789', 'Berry Circle', 'Maine');
+
+insert into Guests_List ('Giver_ID', 'Receiver_ID', 'Event_ID', 'Coming', 'Gifting') 
+	             values (3           1,             1,          1,        1);
+insert into Guests_List ('Giver_ID', 'Receiver_ID', 'Event_ID', 'Coming', 'Gifting') 
+	             values (2,          1,             1,          1,        1);
+insert into Guests_List ('Giver_ID', 'Receiver_ID', 'Event_ID', 'Coming', 'Gifting') 
+				 values (4, 		 2,				2,			0,		  1);
+insert into Guests_List ('Giver_ID', 'Receiver_ID', 'Event_ID', 'Coming', 'Gifting') 
+	             values (5,          2,             2,          1,        0);
+
+
+insert into Events ('User_ID', 'Event_Name',     'Event_Date', 'Location_ID') 
+			values (1,         'Bills Birthday', '2013-04-21', 1);
+insert into Events ('User_ID', 'Event_Name',     'Event_Date', 'Location_ID') 
+			values (2,  	   'Bill Birthday',  '2013-04-21', 2);
+
+
+insert into Gift ('Item_ID', 'Event_ID', 'Giver_ID', 'Price', 'Quantity') 
+	  	  values ('1',       1,          3,          100,	  5); 
+insert into Gift ('Item_ID', 'Event_ID', 'Giver_ID', 'Price', 'Quantity') 
+		  values ('2',       2,          4,          15,      4);
+insert into Gift ('Item_ID', 'Event_ID', 'Giver_ID', 'Price', 'Quantity') 
+		  values ('3',       1,          2,          115,     1);
+insert into Gift ('Item_ID', 'Event_ID', 'Giver_ID', 'Price', 'Quantity') 
+	      values ('4',       2,          5,          5,       20);
+
+
+insert into Item ('Item_ID', 'Department',  'Title',     'Classification') 
+		  values ('1',       'Electronics', 'Xbox 360',  'Elite');
+insert into Item ('Item_ID', 'Department',  'Title',     'Classification') 
+		  values ('2',       'Clothes',     'T-Shirts',  'Medium');
+insert into Item ('Item_ID', 'Department',  'Title',     'Classification') 
+	      values ('3',       'Music',       'Guitar',    'Bass');
+insert into Item ('Item_ID', 'Department',  'Title',     'Classification') 
+		  values ('4',       'Groceries',   'Chocolate', 'Milk');
+
+
+insert into Contact_Info ('User_ID', 'Location_ID',  'Phone-Num',    'Email')
+                  values (1,         1,              '440-568-9124', 'bill10@cox.net');
+insert into Contact_Info ('User_ID', 'Location_ID',  'Phone-Num',    'Email') 
+	              values (2,         2,              '440-569-8742', 'laverne@cox.net');
+insert into Contact_Info ('User_ID', 'Location_ID',  'Phone-Num',    'Email') 
+	              values (3,         3,              '440-578-9612', 'hi1234@yahoo.com');
+insert into Contact_Info ('User_ID', 'Location_ID',  'Phone-Num',    'Email') 
+	              values (4,         4,              '216-852-9461', 'ti@cox.net');
+insert into Contact_Info ('User_ID', 'Location_ID',  'Phone-Num',    'Email') 
+	              values (5,         5,              '216-963-4561', 'Imlovingit@yahoo.com');
+
+
+insert into Address ('Location_Name', 'City',      'Street_Name', 'Street_Number', 'Street_Ext',       'State') 
+	         values ('Home',          'Cleveland', 'Glenside',    93,              'Brunswick Circle', 'Ohio');
+insert into Address ('Location_Name', 'City',      'Street_Name', 'Street_Number', 'Street_Ext',       'State') 
+	         values ('Home', 		  'Cleveland', 'Glenside',    2697,            'Brunswick Circle', 'Ohio');
+insert into Address ('Location_Name', 'City',      'Street_Name', 'Street_Number', 'Street_Ext',       'State') 
+		     values ('Home',          'Cleveland', 'Glenside',    26983,           'Brunswick Circle', 'Ohio');
+insert into Address ('Location_Name', 'City',      'Street_Name', 'Street_Number', 'Street_Ext',       'State') 
+			 values ('Work',          'Toledo',    'Sterns',      26993,           'Brunswick Circle', 'Ohio');
+insert into Address ('Location_Name', 'City',      'Street_Name', 'Street_Number', 'Street_Ext',       'State') 
+			 values ('Home',          'Cleveland', 'Glenside',    26789,           'Berry Circle',     'Maine');
 
 
 
