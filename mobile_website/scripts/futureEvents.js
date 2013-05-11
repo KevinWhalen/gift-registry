a358da721f45385d3eccb6290cf2b1b3809bee1a
widma1 <widma1@yahoo.com>
2013-05-10 20:38:05 -0400

    local repo updating

--- mobile_website/scripts/futureEvents.js
+++ mobile_website/scripts/futureEvents.js
@@ -1,8 +1,16 @@
 function addToFutureDisplay(e)
 {
 	$("#my_future").append('\
+
+
+
 		<div data-role="collapsible-set">\
 			<div data-role="collapsible" data-collapsed="true">\
+
+<div data-role="collapsible" data-collapsed="true" class="ui-collapsible ui-collapsible-inset ui-collapsible-collapsed ui-first-child ui-last-child"><h3 class="ui-collapsible-heading ui-collapsible-heading-collapsed"><a href="#" class="ui-collapsible-heading-toggle ui-btn ui-btn-icon-left ui-btn-up-c" data-corners="false" data-shadow="false" data-iconshadow="true" data-wrapperels="span" data-icon="plus" data-iconpos="left" data-theme="c"><span class="ui-btn-inner"><span class="ui-btn-text">
+                            Event_Name_Date
+                        <span class="ui-collapsible-heading-status"> click to expand contents</span></span>                    
+
 				<h3>'+e.Event_Name+' | '+e.Event_Date+'</h3>\
 				<form action="">\
 					<h2>'+e.Location_Name+'</h2>\
@@ -12,12 +20,33 @@ function addToFutureDisplay(e)
 				</form>\
 			</div>\
 		</div>\
-	').trigger("create");
+	');
 }
 
 $(document).ready(function(){
 
+//$("#my_future_button").click(function(){console.log("click");});
 $("#my_future_button").click(function(){
+/*
+	var futureEvents = 
+			[
+				{
+					"name": "Birthday",
+					"date": "07-16-2013",
+					"loc": "My House - full address from database"
+				},
+				{
+					"name": "Kick ass party",
+					"date": "05-11-2013",
+					"loc": "Kyle's House"
+				},
+				{
+					"name": "Baby Shower",
+					"date": "05-12-2013",
+					"loc": "Library"
+				}
+			];
+*/
 
 	var User_ID = 1;
 
@@ -29,7 +58,6 @@ $("#my_future_button").click(function(){
 		})
 		.done(function(futureEvents){ 
 			futureEvents.forEach(function(e){
-				$("#my_future").empty();
 				addToFutureDisplay(e);
 				console.log(e);
 			});
